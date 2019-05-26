4aba014bb63ed76c14cff15156bb6dbd19245c8c
Ranika Williams <rnkprice07@gmail.com>
2019-05-26 02:02:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
     @@genres.uniq
   end
 
-  def self.genres_count
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre] #if the genre already in hash
