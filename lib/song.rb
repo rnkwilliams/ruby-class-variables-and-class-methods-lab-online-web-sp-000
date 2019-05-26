d65ec8a0075cf4cb39cc817772b6f2ae05822ea7
Ranika Williams <rnkprice07@gmail.com>
2019-05-26 02:05:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,14 @@ class Song
   end
 
   def self.artist_count
-
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] #if the artist already in the hash
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 end
